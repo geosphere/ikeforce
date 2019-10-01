54d4be1d008ce11303caddd558e74d1c71feb705
Kir Vaznitcky <dober@dobergroup.org.ua>
2019-10-01 16:48:43 +0300

    Update ikeclient.py
    fix for error: AttributeError: 'module' object has no attribute 'rand'
    OpenSSL.rand() is obsolete and should be replaced by os.urandom().

--- ikeclient.py
+++ ikeclient.py
@@ -6,6 +6,7 @@
 #You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import socket,OpenSSL,sys,struct
+import os
 try:
         import udp
 except:
@@ -43,7 +44,7 @@ class IKEv1Client(object):
     def secRandom(self, bytes):
 	#Creates selected number of random bytes
 	#Provide the number of bytes required as input, method will return raw bytes
-	randomBytes = OpenSSL.rand.bytes(bytes)
+	randomBytes = os.urandom(bytes)
 	return randomBytes
 
     def payBuild(self, strPayload, lenLen, *arg):
